c711d296a70f17900c2c962d88eb3c05b0d97a29
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-07-01 12:52:52 +0100

    displayed items

--- news/src/App.js
+++ news/src/App.js
@@ -11,10 +11,11 @@ function App() {
     const fetchedData = async () => {
       try {
         const data = await fetch(
-          `https://gnews.io/api/v4/search?q=example&token=${apiKey}&lang=en`
+          `https://gnews.io/api/v4/search?q=${category}&token=${apiKey}&lang=en`
         );
         const articles = await data.json();
-        console.log(articles);
+        console.log(articles.articles);
+        setArticles(articles.articles);
       } catch (error) {
         console.log(error);
       }
@@ -22,7 +23,30 @@ function App() {
     fetchedData();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {articles.map((article, index) => {
+        const { title, description, content, image, publishedAt, url } =
+          article;
+        const str = publishedAt.replace("T", " ");
+        const datePublished = str.replace("Z", " ");
+        return (
+          <div className="grid-container" key={index}>
+            <div className="grid-card">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <img src={image} alt="" />
+              <p>{content}</p>
+              <h4>{datePublished} </h4>
+              <a href={url} target="_blank">
+                Read more
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
