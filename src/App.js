f315b64eca633f94c1e3dc35d23e830601a9ef73
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-07-04 12:49:34 +0100

    search is not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 import "./App.css";
+import Search from "./Components/Search.js";
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -17,55 +18,61 @@ function App() {
         const articles = await data.json();
         console.log(articles.articles);
         setArticles(articles.articles);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
     fetchedData();
-  }, []);
+  }, [category]);
 
   return (
     <>
       <div className="showcase">
         <div className="overlay">
-          <h1 className="text-center mb-4">
+          <h1 className="text-center mb-4 lg:text-6xl">
             {/* Capitalizing category */}
             {category.charAt(0).toLocaleUpperCase() + category.slice(1)} News
           </h1>
+          <Search seacrhText={(text) => setCategory(text)} />
         </div>
       </div>
-      <div className="grid-container ">
-        {articles.map((article, index) => {
-          const { title, description, content, image, publishedAt, url } =
-            article;
-          const str = publishedAt.replace("T", " ");
-          const datePublished = str.replace("Z", " ");
-          return (
-            <div className="articles-container" key={index}>
-              <div className="grid-card">
-                <h2 className="title">{title}</h2>
-                <p className="description">{description}</p>
-                <img src={image} alt="" />
-                <p>
-                  {article?.content?.substring(
-                    0,
-                    article?.content?.length - 15
-                  )}
-                  ...{" "}
-                </p>
-                <h4 className="date">
-                  Published at <span id="date"> {datePublished}</span>{" "}
-                </h4>
-                <button className="btn-read">
-                  <a href={url} target="_blank">
-                    Read more
-                  </a>
-                </button>
+      {isLoading ? (
+        <h1 className="text-center mt-20 font-bold text-6xl">Loading....</h1>
+      ) : (
+        <div className="grid-container ">
+          {articles.map((article, index) => {
+            const { title, description, content, image, publishedAt, url } =
+              article;
+            const str = publishedAt.replace("T", " ");
+            const datePublished = str.replace("Z", " ");
+            return (
+              <div className="articles-container" key={index}>
+                <div className="grid-card">
+                  <h2 className="title lg:text-4xl">{title}</h2>
+                  <p className="description">{description}</p>
+                  <img src={image} alt="" />
+                  <p>
+                    {article?.content?.substring(
+                      0,
+                      article?.content?.length - 15
+                    )}
+                    ...{" "}
+                  </p>
+                  <h4 className="date">
+                    Published at <span id="date"> {datePublished}</span>{" "}
+                  </h4>
+                  <button className="btn-read">
+                    <a href={url} target="_blank">
+                      Read more
+                    </a>
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Search.css";
 
-export default function Search() {
-  return <div>Search</div>;
+export default function Search({ searchText }) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchText(text);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g. sport"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit" className="btn-search">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
